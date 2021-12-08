801cf60ffd7e28044cb972c4c7b7f695d190e622
MaxSapeyko <maks.sapeyko22@ukr.net>
2021-12-08 23:23:45 +0200

    implemented login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,6 +4,8 @@ import { Form, Input, Button } from 'antd';
 import Logo from '../../components/icons/Logo';
 
 import useStyles from './style';
+import Phone from '../../components/icons/Phone';
+import Mail from '../../components/icons/Mail';
 
 const Login: FC = () => {
   const classes = useStyles();
@@ -11,21 +13,45 @@ const Login: FC = () => {
   return (
     <div className={classes.root}>
       <div className={classes.form}>
-        <Logo />
-        <h2>Автоматизована інформаційна система супроводу навчання школярів</h2>
-        <p>Для входу в систему заповніть наступну форму</p>
+        <Logo className='logo__icon' />
+        <h2 className='form__title'>
+          Автоматизована інформаційна система супроводу навчання школярів
+        </h2>
+        <p className='form__description'>
+          Для входу в систему заповніть наступну форму
+        </p>
 
         <Form>
           <Form.Item name='email'>
-            <Input />
+            <Input placeholder='Email' className='form__item' />
           </Form.Item>
 
           <Form.Item name='password'>
-            <Input.Password />
+            <Input.Password placeholder='Password' className='form__item' />
           </Form.Item>
 
-          <Button htmlType='submit'>Увійти</Button>
+          <Button htmlType='submit' type='primary' className='submit__btn'>
+            Увійти
+          </Button>
         </Form>
+      </div>
+
+      <div className={classes.footer}>
+        <span className='help__text'>
+          Якщо виникли труднощі з авторизацією, зверніться в дирекцію школи
+        </span>
+
+        <div className='help__items'>
+          <a href='tel:(0472) 33-07-59' className='help__item'>
+            <Phone />
+            <span className='help__item-text'>(0472) 33-07-59</span>
+          </a>
+
+          <a href='mailto:best_school@gmail.com' className='help__item'>
+            <Mail />
+            <span className='help__item-text'>best_school@gmail.com</span>
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/pages/login/style.ts
+++ src/pages/login/style.ts
@@ -1,4 +1,5 @@
 import { createUseStyles } from 'react-jss';
+import { COLORS } from '../../utils/common';
 
 const useStyles = createUseStyles({
   root: {
@@ -8,7 +9,7 @@ const useStyles = createUseStyles({
     alignContent: 'center',
     minHeight: '100vh',
     width: '100%',
-    maxWidth: '13.2rem'
+    padding: '0 1rem 14.8rem',
   },
 
   form: {
@@ -16,7 +17,79 @@ const useStyles = createUseStyles({
     flexDirection: 'column',
     justifyContent: 'center',
     alignContent: 'center',
-  }
+    width: '100%',
+    textAlign: 'center',
+
+    '& .logo__icon': {
+      margin: 'auto',
+    },
+
+    '& .form__title': {
+      margin: '6rem auto 3rem',
+      maxWidth: '71rem',
+      fontSize: '3.2rem',
+      fontWeight: '700',
+      lineHeight: '1.2',
+    },
+
+    '& .form__description': {
+      marginBottom: '6rem',
+      fontSize: '1.7rem',
+      lineHeight: '1.2',
+    },
+
+    '& .ant-form-item-control': {
+      margin: 'auto',
+      maxWidth: '32rem',
+
+      '& .form__item': {
+        height: '5.8rem',
+      },
+    },
+
+    '& .submit__btn': {
+      width: '32rem',
+      height: '5.8rem',
+      backgroundColor: COLORS.blue,
+    },
+  },
+
+  footer: {
+    maxWidth: '134rem',
+    width: 'calc(100% - 2rem)',
+    position: 'fixed',
+    left: '50%',
+    bottom: '0',
+    borderTop: `1px solid ${COLORS.dividers}`,
+    padding: '3rem 0',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    transform: 'translate(-50%, 0)',
+
+    '& .help__text': {
+      display: 'inline-block',
+      fontSize: '1.5rem',
+      lineHeight: '1.3',
+    },
+
+    '& .help__items': {
+      display: 'flex',
+      gap: '0 4.2rem',
+      alignItems: 'center',
+    },
+
+    '& .help__item': {
+      display: 'flex',
+      alignItems: 'center',
+
+      '& .help__item-text': {
+        fontWeight: '500',
+        color: COLORS.black,
+        marginLeft: '0.8rem',
+      },
+    },
+  },
 });
 
 export default useStyles;
