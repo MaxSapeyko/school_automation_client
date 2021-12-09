ee1503206c50e3d549801af6e27a6e1ca05a6480
MaxSapeyko <maks.sapeyko22@ukr.net>
2021-12-09 23:49:03 +0200

    implemented sidebar manu

--- src/components/layout/sider/style.ts
+++ src/components/layout/sider/style.ts
@@ -3,10 +3,48 @@ import { COLORS } from '../../../utils/common';
 
 const useStyles = createUseStyles({
   root: {
-    heigth: '100vh',
+    height: '100vh',
     width: '28rem',
     backgroundColor: COLORS.blue,
     color: COLORS.white,
+
+    '& .logo__icon': {
+      width: '8rem',
+      height: '4.9rem',
+      margin: '4.6rem auto',
+      display: 'block',
+
+      '& path': {
+        fill: COLORS.white,
+      },
+    },
+  },
+
+  menu: {
+    backgroundColor: 'transparent',
+    color: COLORS.white,
+    fontWeight: '600',
+    fontSize: '1.5rem',
+
+    '&.ant-menu:not(.ant-menu-horizontal) .ant-menu-item-selected': {
+      color: COLORS.white,
+      backgroundColor: COLORS.emerald,
+    },
+
+    '& .ant-menu-item-active': {
+      color: `${COLORS.white} !important`,
+      backgroundColor: COLORS.emerald,
+    },
+
+    '& .ant-menu-item::after': {
+      display: 'none',
+    },
+
+    '& .logout__btn': {
+      position: 'absolute',
+      bottom: '2rem',
+      width: '28rem',
+    },
   },
 });
 
