cb6cf58daebabe8a3bf5e8da3dff21225489c2e6
MaxSapeyko <maks.sapeyko22@ukr.net>
2021-12-22 10:47:55 +0200

    get user data method

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -10,6 +10,7 @@ import PersonalData from './personalData';
 import { USER_MOCK } from '../../MOCK/user';
 
 import useStyles from './style';
+import { userService } from '../../services/userService';
 
 export interface ProfileProps {
   type: 'own' | 'teacher' | 'pupil';
@@ -19,10 +20,18 @@ const Profile: FC<ProfileProps> = ({ type }) => {
   const classes = useStyles();
   const [form] = useForm();
 
-  useEffect(() => {
+  const getUser = async (userId: string) => {
+    const user = userService.userById(userId);
+    console.log(user);
+
     form.setFieldsValue({
-      ...USER_MOCK,
+      ...user,
     });
+  };
+
+  useEffect(() => {
+    getUser();
+
     // eslint-disable-next-line
   }, []);
 
